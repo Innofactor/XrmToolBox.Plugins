4d06b2bcfba96537b6a063aea46072dbfbc7957c
Alexey Shytikov <alexey.shytikov@cinteros.se>
2015-01-23 10:38:28 +0100

    Starting accessing lis or available connections

--- Environments.Compare/EnvironmentsSelector.Designer.cs
+++ Environments.Compare/EnvironmentsSelector.Designer.cs
@@ -30,10 +30,20 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EnvironmentsSelector));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lvReference = new System.Windows.Forms.ListView();
+            this.chRefOrgName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chRefOrgUrl = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lvOrganizations = new System.Windows.Forms.ListView();
+            this.chOrgName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chOrgUrl = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.tsbClose = new System.Windows.Forms.ToolStripButton();
             this.tsbCompare = new System.Windows.Forms.ToolStripButton();
+            this.chRefVersion = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chOrgVersion = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -42,24 +52,78 @@
             this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox1.Controls.Add(this.lvReference);
             this.groupBox1.Location = new System.Drawing.Point(7, 34);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(475, 100);
+            this.groupBox1.Size = new System.Drawing.Size(475, 89);
             this.groupBox1.TabIndex = 8;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Reference Organization";
+            // 
+            // lvReference
+            // 
+            this.lvReference.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvReference.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chRefOrgName,
+            this.chRefOrgUrl,
+            this.chRefVersion});
+            this.lvReference.Location = new System.Drawing.Point(7, 20);
+            this.lvReference.Name = "lvReference";
+            this.lvReference.Size = new System.Drawing.Size(462, 63);
+            this.lvReference.TabIndex = 0;
+            this.lvReference.UseCompatibleStateImageBehavior = false;
+            this.lvReference.View = System.Windows.Forms.View.Details;
+            // 
+            // chRefOrgName
+            // 
+            this.chRefOrgName.Text = "Organization";
+            this.chRefOrgName.Width = 200;
+            // 
+            // chRefOrgUrl
+            // 
+            this.chRefOrgUrl.Text = "URL";
+            this.chRefOrgUrl.Width = 500;
             // 
             // groupBox2
             // 
             this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.groupBox2.Location = new System.Drawing.Point(7, 140);
+            this.groupBox2.Controls.Add(this.lvOrganizations);
+            this.groupBox2.Location = new System.Drawing.Point(7, 129);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(475, 279);
+            this.groupBox2.Size = new System.Drawing.Size(475, 290);
             this.groupBox2.TabIndex = 9;
             this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Organizations (select to compare)";
+            this.groupBox2.Text = "Organizations to compare";
+            // 
+            // lvOrganizations
+            // 
+            this.lvOrganizations.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvOrganizations.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chOrgName,
+            this.chOrgUrl,
+            this.chOrgVersion});
+            this.lvOrganizations.Location = new System.Drawing.Point(6, 19);
+            this.lvOrganizations.Name = "lvOrganizations";
+            this.lvOrganizations.Size = new System.Drawing.Size(462, 265);
+            this.lvOrganizations.TabIndex = 2;
+            this.lvOrganizations.UseCompatibleStateImageBehavior = false;
+            this.lvOrganizations.View = System.Windows.Forms.View.Details;
+            // 
+            // chOrgName
+            // 
+            this.chOrgName.Text = "Organization";
+            this.chOrgName.Width = 200;
+            // 
+            // chOrgUrl
+            // 
+            this.chOrgUrl.Text = "URL";
+            this.chOrgUrl.Width = 500;
             // 
             // toolStrip1
             // 
@@ -90,6 +154,16 @@
             this.tsbCompare.Text = "Compare solutions";
             this.tsbCompare.Click += new System.EventHandler(this.tsbCompare_Click);
             // 
+            // chRefVersion
+            // 
+            this.chRefVersion.Text = "Version";
+            this.chRefVersion.Width = 100;
+            // 
+            // chOrgVersion
+            // 
+            this.chOrgVersion.Text = "Version";
+            this.chOrgVersion.Width = 100;
+            // 
             // EnvironmentsSelector
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -99,6 +173,9 @@
             this.Controls.Add(this.groupBox1);
             this.Name = "EnvironmentsSelector";
             this.Size = new System.Drawing.Size(496, 439);
+            this.Load += new System.EventHandler(this.EnvironmentsSelector_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -113,5 +190,13 @@
         private System.Windows.Forms.ToolStrip toolStrip1;
         private System.Windows.Forms.ToolStripButton tsbClose;
         private System.Windows.Forms.ToolStripButton tsbCompare;
+        private System.Windows.Forms.ListView lvReference;
+        private System.Windows.Forms.ColumnHeader chRefOrgName;
+        private System.Windows.Forms.ListView lvOrganizations;
+        private System.Windows.Forms.ColumnHeader chOrgName;
+        private System.Windows.Forms.ColumnHeader chOrgUrl;
+        private System.Windows.Forms.ColumnHeader chRefOrgUrl;
+        private System.Windows.Forms.ColumnHeader chRefVersion;
+        private System.Windows.Forms.ColumnHeader chOrgVersion;
     }
 }
--- Environments.Compare/EnvironmentsSelector.cs
+++ Environments.Compare/EnvironmentsSelector.cs
@@ -1,21 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using XrmToolBox;
-
-namespace Environments.Compare
+﻿namespace Environments.Compare
 {
-    public partial class EnvironmentsSelector : PluginBase  
+    using McTools.Xrm.Connection;
+    using Microsoft.Xrm.Sdk.Query;
+    using System;
+    using System.Windows.Forms;
+    using XrmToolBox;
+
+    public partial class EnvironmentsSelector : PluginBase
     {
-        public EnvironmentsSelector()
+        #region Private Methods
+
+        private void InitializeControls()
         {
-            InitializeComponent();
+            this.MinimumSize = new System.Drawing.Size(600, 400);
+
+            // base.ConnectionDetail.
+            if (base.ConnectionDetail != null)
+            {
+                lvReference.Items.Add(new ListViewItem(
+                    new string[] {
+                    this.ConnectionDetail.OrganizationFriendlyName,
+                    this.ConnectionDetail.OrganizationServiceUrl,
+                    this.ConnectionDetail.OrganizationVersion
+                }));
+            }
+
+            foreach (var connection in new ConnectionManager().ConnectionsList.Connections)
+            {
+                var item = new ListViewItem(
+                    new string[] {
+                    connection.OrganizationFriendlyName,
+                    connection.OrganizationServiceUrl,
+                    connection.OrganizationVersion
+                    });
+            }
+        }
+
+        private void LoadRefereceSolutions()
+        {
+            this.WorkAsync("Retrieving your user id...",
+                (e) => // Work To Do Asynchronously
+                {
+                    var query = new QueryExpression("solution");
+                    query.Criteria = new FilterExpression();
+                    query.Criteria.AddCondition("isvisible", ConditionOperator.Equal, true);
+                    query.ColumnSet = new ColumnSet(new string[] { "friendlyname", "version" });
+
+                    e.Result = this.Service.RetrieveMultiple(query).Entities;
+                },
+                e =>  // Cleanup when work has completed
+                {
+                    // MessageBox.Show(string.Format("You are {0}", (Guid)e.Result));
+                }
+            );
         }
 
         private void tsbClose_Click(object sender, EventArgs e)
@@ -25,7 +62,23 @@ namespace Environments.Compare
 
         private void tsbCompare_Click(object sender, EventArgs e)
         {
+            this.LoadRefereceSolutions();
+        }
 
+        #endregion Private Methods
+
+        #region Public Constructors
+
+        public EnvironmentsSelector()
+        {
+            InitializeComponent();
+        }
+
+        #endregion Public Constructors
+
+        private void EnvironmentsSelector_Load(object sender, EventArgs e)
+        {
+            this.InitializeControls();
         }
     }
 }
