d7124ea6491c34c03a61cec4e3c53cd7e6c27491
Alexey Shytikov <alexey.shytikov@cinteros.se>
2015-02-13 17:06:41 +0100

    Adding OrganizationDetail class

---
+++ SolutionVerifier/Utils/OrganizationDetail.cs
@@ -0,0 +1,24 @@
+﻿namespace Cinteros.Xrm.SolutionVerifier.Utils
+{
+    using McTools.Xrm.Connection;
+
+    public class OrganizationDetail
+    {
+        public OrganizationDetail(ConnectionDetail connectionDetail, Solution[] solutions)
+        {
+            this.ConnectionDetail = connectionDetail;
+            this.Solutions = solutions;
+        }
+
+        #region Public Fields
+
+        public ConnectionDetail ConnectionDetail;
+
+        /// <summary>
+        /// Array of solutions available in the organization
+        /// </summary>
+        public Solution[] Solutions;
+
+        #endregion Public Fields
+    }
+}
