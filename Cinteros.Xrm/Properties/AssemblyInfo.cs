bd676ca84c18d1fcc4b8f6654e3d878c6f0dbfcc
Alexey Shytikov <alexey.shytikov@cinteros.se>
2015-07-07 01:01:21 +0200

    Organizing reporitory

--- Cinteros.Xrm/Properties/AssemblyInfo.cs
+++ Cinteros.Xrm/Properties/AssemblyInfo.cs
@@ -5,12 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("XrmToolBox.Plugins")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Cinteros.XrmToolBox.Plugins")]
+[assembly: AssemblyDescription("Cinteros XrmToolBox Plugins")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("XrmToolBox.Plugins")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("Cinteros.XrmToolBox.Plugins")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +30,3 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
