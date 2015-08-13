f59d17cdad90c5b0cab27209a48c971f7425bcfb
Alexey Shytikov <alexey.shytikov@cinteros.se>
2015-08-13 16:02:53 +0200

    Disabling name of the view

--- Cinteros.Xrm/ViewDesignerTool/Controls/ViewEditor.Designer.cs
+++ Cinteros.Xrm/ViewDesignerTool/Controls/ViewEditor.Designer.cs
@@ -102,6 +102,7 @@
             // 
             // tbName
             // 
+            this.tbName.Enabled = false;
             this.tbName.Location = new System.Drawing.Point(48, 17);
             this.tbName.Name = "tbName";
             this.tbName.Size = new System.Drawing.Size(272, 20);
