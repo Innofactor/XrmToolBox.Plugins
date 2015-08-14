481e4ac70726b07c623b9ec654c3197ecb734958
Alexey Shytikov <alexey.shytikov@cinteros.se>
2015-08-14 10:48:06 +0200

    Removing panel from ViewXml form

--- Cinteros.Xrm/Common/Forms/XmlContentDisplayDialog.Designer.cs
+++ Cinteros.Xrm/Common/Forms/XmlContentDisplayDialog.Designer.cs
@@ -36,15 +36,14 @@
             this.panCancel = new System.Windows.Forms.Panel();
             this.btnCancel = new System.Windows.Forms.Button();
             this.btnFormat = new System.Windows.Forms.Button();
-            this.txtXML = new CSRichTextBoxSyntaxHighlighting.XMLViewer();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
-            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.gbXml = new System.Windows.Forms.GroupBox();
+            this.txtXML = new CSRichTextBoxSyntaxHighlighting.XMLViewer();
             this.panBottom.SuspendLayout();
             this.panOk.SuspendLayout();
             this.panCancel.SuspendLayout();
             this.panel1.SuspendLayout();
+            this.gbXml.SuspendLayout();
             this.SuspendLayout();
             // 
             // panBottom
@@ -54,9 +53,9 @@
             this.panBottom.Controls.Add(this.panCancel);
             this.panBottom.Controls.Add(this.btnFormat);
             this.panBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panBottom.Location = new System.Drawing.Point(0, 523);
+            this.panBottom.Location = new System.Drawing.Point(0, 533);
             this.panBottom.Name = "panBottom";
-            this.panBottom.Size = new System.Drawing.Size(784, 38);
+            this.panBottom.Size = new System.Drawing.Size(784, 28);
             this.panBottom.TabIndex = 4;
             // 
             // panOk
@@ -65,13 +64,13 @@
             this.panOk.Dock = System.Windows.Forms.DockStyle.Right;
             this.panOk.Location = new System.Drawing.Point(602, 0);
             this.panOk.Name = "panOk";
-            this.panOk.Size = new System.Drawing.Size(91, 38);
+            this.panOk.Size = new System.Drawing.Size(91, 28);
             this.panOk.TabIndex = 4;
             // 
             // btnOk
             // 
             this.btnOk.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.btnOk.Location = new System.Drawing.Point(3, 8);
+            this.btnOk.Location = new System.Drawing.Point(16, 0);
             this.btnOk.Name = "btnOk";
             this.btnOk.Size = new System.Drawing.Size(75, 23);
             this.btnOk.TabIndex = 3;
@@ -85,13 +84,13 @@
             this.panCancel.Dock = System.Windows.Forms.DockStyle.Right;
             this.panCancel.Location = new System.Drawing.Point(693, 0);
             this.panCancel.Name = "panCancel";
-            this.panCancel.Size = new System.Drawing.Size(91, 38);
+            this.panCancel.Size = new System.Drawing.Size(91, 28);
             this.panCancel.TabIndex = 3;
             // 
             // btnCancel
             // 
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(3, 8);
+            this.btnCancel.Location = new System.Drawing.Point(13, 0);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(75, 23);
             this.btnCancel.TabIndex = 4;
@@ -101,7 +100,7 @@
             // 
             // btnFormat
             // 
-            this.btnFormat.Location = new System.Drawing.Point(12, 8);
+            this.btnFormat.Location = new System.Drawing.Point(6, 0);
             this.btnFormat.Name = "btnFormat";
             this.btnFormat.Size = new System.Drawing.Size(75, 23);
             this.btnFormat.TabIndex = 2;
@@ -109,11 +108,33 @@
             this.btnFormat.UseVisualStyleBackColor = true;
             this.btnFormat.Click += new System.EventHandler(this.button3_Click);
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.gbXml);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(784, 530);
+            this.panel1.TabIndex = 5;
+            // 
+            // gbXml
+            // 
+            this.gbXml.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbXml.Controls.Add(this.txtXML);
+            this.gbXml.Location = new System.Drawing.Point(3, 3);
+            this.gbXml.Name = "gbXml";
+            this.gbXml.Size = new System.Drawing.Size(778, 524);
+            this.gbXml.TabIndex = 0;
+            this.gbXml.TabStop = false;
+            this.gbXml.Text = "XML";
+            // 
             // txtXML
             // 
             this.txtXML.Dock = System.Windows.Forms.DockStyle.Fill;
             this.txtXML.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtXML.Location = new System.Drawing.Point(0, 57);
+            this.txtXML.Location = new System.Drawing.Point(3, 16);
             this.txtXML.Name = "txtXML";
             xmlViewerSettings1.AttributeKey = System.Drawing.Color.Red;
             xmlViewerSettings1.AttributeValue = System.Drawing.Color.Blue;
@@ -122,57 +143,9 @@
             xmlViewerSettings1.Tag = System.Drawing.Color.Blue;
             xmlViewerSettings1.Value = System.Drawing.Color.Black;
             this.txtXML.Settings = xmlViewerSettings1;
-            this.txtXML.Size = new System.Drawing.Size(784, 466);
-            this.txtXML.TabIndex = 1;
+            this.txtXML.Size = new System.Drawing.Size(772, 505);
+            this.txtXML.TabIndex = 2;
             this.txtXML.Text = "";
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.linkLabel2);
-            this.panel1.Controls.Add(this.linkLabel1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(784, 57);
-            this.panel1.TabIndex = 5;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Lucida Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 14);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(300, 15);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Uncomfortable hand-writing fetch xml queries?";
-            // 
-            // linkLabel2
-            // 
-            this.linkLabel2.AutoSize = true;
-            this.linkLabel2.Font = new System.Drawing.Font("Lucida Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.linkLabel2.LinkArea = new System.Windows.Forms.LinkArea(4, 16);
-            this.linkLabel2.Location = new System.Drawing.Point(13, 31);
-            this.linkLabel2.Name = "linkLabel2";
-            this.linkLabel2.Size = new System.Drawing.Size(331, 20);
-            this.linkLabel2.TabIndex = 2;
-            this.linkLabel2.TabStop = true;
-            this.linkLabel2.Text = "Use FetchXML Builder to compose it in a nice GUI !";
-            this.linkLabel2.UseCompatibleTextRendering = true;
-            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
-            // 
-            // linkLabel1
-            // 
-            this.linkLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.linkLabel1.Location = new System.Drawing.Point(513, 22);
-            this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(258, 16);
-            this.linkLabel1.TabIndex = 1;
-            this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "http://fxb.xrmtoolbox.com";
-            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
             // XmlContentDisplayDialog
             // 
@@ -180,7 +153,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnCancel;
             this.ClientSize = new System.Drawing.Size(784, 561);
-            this.Controls.Add(this.txtXML);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.panBottom);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -193,7 +165,7 @@
             this.panOk.ResumeLayout(false);
             this.panCancel.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.gbXml.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -205,11 +177,9 @@
         private System.Windows.Forms.Button btnOk;
         private System.Windows.Forms.Panel panCancel;
         private System.Windows.Forms.Button btnCancel;
-        internal CSRichTextBoxSyntaxHighlighting.XMLViewer txtXML;
         internal System.Windows.Forms.Button btnFormat;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.LinkLabel linkLabel1;
-        private System.Windows.Forms.LinkLabel linkLabel2;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox gbXml;
+        internal CSRichTextBoxSyntaxHighlighting.XMLViewer txtXML;
     }
 }
