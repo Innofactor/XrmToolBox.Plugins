de0526217a86d711d9c947843e6ef1e88eb0a3a7
Alexey Shytikov <alexey.shytikov@cinteros.se>
2015-08-24 13:16:13 +0200

    Correcting issue with metadata export

--- Cinteros.Xrm/AutoDeployTool/PluginDescription.cs
+++ Cinteros.Xrm/AutoDeployTool/PluginDescription.cs
@@ -10,7 +10,8 @@
     ExportMetadata("Name", "Plugin Auto Deployer"),
     ExportMetadata("Description", "Tool to automatically upload plugin assemblies if they are changed during build process"),
     ExportMetadata("SmallImageBase64", Constants.B64_IMAGE_SMALL), // null for "no logo" image or base64 image content
-    ExportMetadata("BigImageBase64", Constants.B64_IMAGE_LARGE), // null for "no logo" image or base64 image content    ExportMetadata("BackgroundColor", "#ffffff"), // Use a HTML color name
+    ExportMetadata("BigImageBase64", Constants.B64_IMAGE_LARGE), // null for "no logo" image or base64 image content
+    ExportMetadata("BackgroundColor", "#ffffff"), // Use a HTML color name
     ExportMetadata("PrimaryFontColor", "#000000"), // Or an hexadecimal code
     ExportMetadata("SecondaryFontColor", "DarkGray")]
     public class AutoDeployTool : PluginBase
