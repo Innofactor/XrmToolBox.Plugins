1a54a7afb40a43121097f8fbcb99b969140fc44c
Alexey Shytikov <alexey.shytikov@cinteros.se>
2015-09-08 22:57:35 +0200

    Adding plugin description for Steps Manipulation Tool

---
+++ Cinteros.Xrm/StepsManipulationTool/PluginDescription.cs
@@ -0,0 +1,28 @@
+﻿namespace Cinteros.Xrm.Plugins.StepsManipulationTool
+{
+    using System.ComponentModel.Composition;
+    using Cinteros.Xrm.Common.Utils;
+    using Cinteros.Xrm.StepsManipulationTool;
+    using XrmToolBox.Extensibility;
+    using XrmToolBox.Extensibility.Interfaces;
+
+    [Export(typeof(IXrmToolBoxPlugin)),
+    ExportMetadata("Name", "Steps Manipulator"),
+    ExportMetadata("Description", "Tool to perform unsafe operations with plugin steps"),
+    ExportMetadata("SmallImageBase64", Constants.B64_IMAGE_SMALL), // null for "no logo" image or base64 image content
+    ExportMetadata("BigImageBase64", Constants.B64_IMAGE_LARGE), // null for "no logo" image or base64 image content
+    ExportMetadata("BackgroundColor", "#ffffff"), // Use a HTML color name
+    ExportMetadata("PrimaryFontColor", "#000000"), // Or an hexadecimal code
+    ExportMetadata("SecondaryFontColor", "DarkGray")]
+    public class StepsManipulationTool : PluginBase
+    {
+        #region Public Methods
+
+        public override IXrmToolBoxPluginControl GetControl()
+        {
+            return new MainControl();
+        }
+
+        #endregion Public Methods
+    }
+}
